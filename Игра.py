639b8a2b698e20a4dcab498fbe6cf5214e2a1c83
AnnZarembo <146215603+AnnZarembo@users.noreply.github.com>
2023-12-23 22:51:28 +0500

    добавлен файл Log

--- Игра.py
+++ Игра.py
@@ -6,22 +6,22 @@ logging.basicConfig(filename='Ugadaika.log', level=logging.INFO, format='%(ascti
 def polsovatel():
     while True:
         try:
-            k=int(input("Введите максимальное число для загадывания: "))
-            n=int(input("Введите количество попыток: "))
-            if n<1 or k<1:
+            n=int(input("Введите максимальное число для загадывания: "))
+            k=int(input("Введите количество попыток: "))
+            if k<1 or n<1:
                 print("Нужно ввести положительные числа, которые больше 0.")
-                logging.error(f"!Нужно ввести положительные числа: {k}, {n}")
+                logging.error(f"!Нужно ввести положительные числа: {n}, {k}")
             else:
-                return k,n
+                return n,k
         except ValueError:
             print("Нужно ввести натуральные числа.")
-            logging.error(f"!Нужно ввести натуральные числа: {k}, {n}")
+            logging.error(f"!Нужно ввести натуральные числа: {n}, {k}")
 
-def play(k,n):
-    secret=random.randint(1, k)
-    logging.info(f'Компьютер загадал число от 1 до {k} ({secret}). Количество попыток: {n}')
-    print(f"Компьютер загадал число от 1 до {k}. Количество попыток: {n}.")
-    for popitka in range(1, n + 1):
+def play(n,k):
+    secret=random.randint(1, n)
+    logging.info(f'Компьютер загадал число от 1 до {n} ({secret}). Количество попыток: {k}')
+    print(f"Компьютер загадал число от 1 до {n}. Количество попыток: {k}.")
+    for popitka in range(1, k + 1):
         dogadka = int(input(f"Попытка {popitka}. Угадайте число: "))
         logging.info(f'Попытка {popitka}: {dogadka}')
         if dogadka > secret:
@@ -37,5 +37,5 @@ def play(k,n):
     print(f"Попытки закончились. Загаданное число: {secret}")
     logging.info('Вы проиграли :(')
 if __name__ == "__main__":
-    k,n = polsovatel()
-    play(k, n)
+    n,k = polsovatel()
+    play(n,k)
